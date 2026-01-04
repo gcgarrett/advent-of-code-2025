88112669f1857e7634b53ad711ad0fc1ab0e4e82
Gregory Garrett <gcgarrett@gmail.com>
2026-01-04 16:59:43 -0500

    Solution for part 2 Adds solution for part 2, which can be run using the `-o` or` --override` command line option Uses a sliding window to find the next highest joltage value The window depends on the number of joltages still required

--- day3/joltage.py
+++ day3/joltage.py
@@ -1,6 +1,20 @@
+import getopt
 import logging
+import sys
+from functools import reduce
 
 logging.basicConfig(level=logging.DEBUG)
+
+JOLTAGE_LIMIT_SAFETY_OVERRIDE = False
+
+# command line arguments
+#   -o --override: overrides the joltage safety limit
+opts, args = getopt.getopt(sys.argv[1:], 'o', ['override'])
+
+for opt, arg in opts:
+    print(opt)
+    if opt in ('-o', '--override'):
+        JOLTAGE_LIMIT_SAFETY_OVERRIDE = True
 
 f = open('./puzzle_input')
 
@@ -9,6 +23,8 @@ total_joltage = 0
 def find_highest(sub_bank):
     highest_index = -1
     highest_battery = -1
+
+    logging.debug(f'Sub bank: {sub_bank}')
 
     for index, battery in enumerate(sub_bank):
         if battery > highest_battery:
@@ -28,6 +44,19 @@ def calculate_joltage(bank):
 
     return (first_battery * 10) + second_battery
 
+def calculate_largest_joltage(bank):
+    joltages = []
+    start_index = 0
+
+    while len(joltages) < 12:
+        remaining = 12 - (len(joltages) + 1)
+        end_index = len(bank) - remaining
+        highest_index, highest_battery = find_highest(bank[start_index:end_index])
+        joltages.append(highest_battery)
+        start_index = start_index + highest_index + 1
+
+    logging.debug(f'Joltages: {joltages}')
+    return reduce(lambda x, y: (x * 10) + y, joltages)
 
 while(True):
     bank_str = f.readline().strip()
@@ -36,6 +65,10 @@ while(True):
         break
 
     bank = list(map(int, list(bank_str)))
-    total_joltage += calculate_joltage(bank)
+
+    if not JOLTAGE_LIMIT_SAFETY_OVERRIDE:
+        total_joltage += calculate_joltage(bank)
+    else:
+        total_joltage += calculate_largest_joltage(bank)
 
 logging.info(f'Total joltage is: {total_joltage}')
